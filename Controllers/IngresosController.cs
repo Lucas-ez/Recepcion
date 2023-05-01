452e326f707939cf80afd074b65ea078b3c41468
patricio97m <patricio.97m@gmail.com>
2023-05-01 10:48:26 -0300

    Controlador modificado

--- Controllers/IngresosController.cs
+++ Controllers/IngresosController.cs
@@ -1,163 +1,155 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Recepcion.Data;
 using Recepcion.Models;
 
 namespace Recepcion.Controllers
 {
-    public class IngresosController : Controller
-    {
-        private readonly RecepcionContext _context;
+	public class IngresosController : Controller
+	{
+		private readonly RecepcionContext _context;
 
-        public IngresosController(RecepcionContext context)
-        {
-            _context = context;
-        }
+		//Se declara la base de datos
+		public IngresosController(RecepcionContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Ingresos
-        public async Task<IActionResult> Index()
-        {
-              return _context.Ingreso != null ? 
-                          View(await _context.Ingreso.ToListAsync()) :
-                          Problem("Entity set 'RecepcionContext.Ingreso'  is null.");
-        }
+		// GET: Ingresos
+		public IActionResult Index()
+		{
+			return _context.Ingreso != null ?
+						View(_context.Ingreso.ToList()) :
+						Problem("Error en la base de datos");
+		}
 
-        // GET: Ingresos/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Ingreso == null)
-            {
-                return NotFound();
-            }
+		// GET: Ingresos/Details/1234
+		public IActionResult Details(int? id)
+		{
+			if (id == null || _context.Ingreso == null)
+			{
+				return NotFound();
+			}
 
-            var ingreso = await _context.Ingreso
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (ingreso == null)
-            {
-                return NotFound();
-            }
+			var ingreso = _context.Ingreso
+				.FirstOrDefault(m => m.Id == id);
+			if (ingreso == null)
+			{
+				return NotFound();
+			}
 
-            return View(ingreso);
-        }
+			return View(ingreso);
+		}
 
-        // GET: Ingresos/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+		// GET: Ingresos/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: Ingresos/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,ClienteDni,ClienteNombre,RoomNumber,CheckInDate,CheckOutDate")] Ingreso ingreso)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(ingreso);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(ingreso);
-        }
+		// POST: Ingresos/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create([Bind("Id,ClienteDni,ClienteNombre,RoomNumber,CheckInDate,CheckOutDate")] Ingreso ingreso)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(ingreso);
+				_context.SaveChanges();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(ingreso);
+		}
 
-        // GET: Ingresos/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Ingreso == null)
-            {
-                return NotFound();
-            }
+		// GET: Ingresos/Edit/1234
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || _context.Ingreso == null)
+			{
+				return NotFound();
+			}
 
-            var ingreso = await _context.Ingreso.FindAsync(id);
-            if (ingreso == null)
-            {
-                return NotFound();
-            }
-            return View(ingreso);
-        }
+			var ingreso = _context.Ingreso.Find(id);
+			if (ingreso == null)
+			{
+				return NotFound();
+			}
+			return View(ingreso);
+		}
 
-        // POST: Ingresos/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,ClienteDni,ClienteNombre,RoomNumber,CheckInDate,CheckOutDate")] Ingreso ingreso)
-        {
-            if (id != ingreso.Id)
-            {
-                return NotFound();
-            }
+		// POST: Ingresos/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(int id, [Bind("Id,ClienteDni,ClienteNombre,RoomNumber,CheckInDate,CheckOutDate")] Ingreso ingreso)
+		{
+			if (id != ingreso.Id)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(ingreso);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!IngresoExists(ingreso.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(ingreso);
-        }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(ingreso);
+					_context.SaveChanges();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!IngresoExists(ingreso.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(ingreso);
+		}
 
-        // GET: Ingresos/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Ingreso == null)
-            {
-                return NotFound();
-            }
+		// GET: Ingresos/Delete/1234
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || _context.Ingreso == null)
+			{
+				return NotFound();
+			}
 
-            var ingreso = await _context.Ingreso
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (ingreso == null)
-            {
-                return NotFound();
-            }
+			var ingreso = _context.Ingreso
+				.FirstOrDefault(m => m.Id == id);
+			if (ingreso == null)
+			{
+				return NotFound();
+			}
 
-            return View(ingreso);
-        }
+			return View(ingreso);
+		}
 
-        // POST: Ingresos/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Ingreso == null)
-            {
-                return Problem("Entity set 'RecepcionContext.Ingreso'  is null.");
-            }
-            var ingreso = await _context.Ingreso.FindAsync(id);
-            if (ingreso != null)
-            {
-                _context.Ingreso.Remove(ingreso);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+		// POST: Ingresos/Delete/1234
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeleteConfirmed(int id)
+		{
+			if (_context.Ingreso == null)
+			{
+				return Problem("La entidad es nula");
+			}
+			var ingreso = _context.Ingreso.Find(id);
+			if (ingreso != null)
+			{
+				_context.Ingreso.Remove(ingreso);
+			}
 
-        private bool IngresoExists(int id)
-        {
-          return (_context.Ingreso?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
-    }
+			_context.SaveChanges();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool IngresoExists(int id)
+		{
+			return (_context.Ingreso?.Any(e => e.Id == id)).GetValueOrDefault();
+		}
+	}
 }
