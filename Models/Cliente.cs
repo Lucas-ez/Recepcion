254ffdfe2569def4dc6ba82989632451c290e733
Lucas <lucasezequiel237@gmail.com>
2023-05-01 09:52:03 -0300

    Add modelos Cliente e Ingreso

---
+++ Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace Recepcion.Models
+{
+  public class Cliente
+  {
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public int Dni { get; set; }
+    public DateTime BirthDate { get; set; }
+  }
+}
---
+++ Models/Ingreso.cs
@@ -0,0 +1,12 @@
+﻿namespace Recepcion.Models
+{
+  public class Ingreso
+  {
+    public int Id { get; set; }
+    public int ClienteId { get; set; }
+    public Cliente Cliente { get; set; }
+    public int RoomNumber { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+  }
+}
